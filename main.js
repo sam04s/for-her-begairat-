3cfef2a1d6612cb0f78921a0ff131ec9f5092e9d
sam04s <inteshamazad846@gmail.com>
2025-01-29 15:10:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+document.addEventListener("DOMContentLoaded", function () {
+    const audio = document.getElementById("backgroundMusic");
+    audio.play().catch(error => {
+      console.log("Autoplay blocked! Click anywhere to play music.");
+    });
+
+    // Optional: Allow user interaction to resume audio if blocked
+    document.body.addEventListener("click", function () {
+      audio.play();
+    }, { once: true });
+  });
